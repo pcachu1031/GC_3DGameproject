c9b6ba9d840dfe4298632e086fe6c521bc5ea887
pcachu1031 <satiy1031@naver.com>
2023-07-18 15:56:17 +0900

    Update Map
    맵 대폭 수정

--- Source/GC_3DGameproject/MyActor.cpp
+++ Source/GC_3DGameproject/MyActor.cpp
@@ -9,8 +9,6 @@ AMyActor::AMyActor()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	//UE_LOG(LogTemp, Log, TEXT("asd");
-	//UE_LOG(LogTemp, Log, TEXT("Constructor"));
 }
 
 // Called when the game starts or when spawned
